7ecc1a98647d2eb1b18675222779d8f13b915b65
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-03-31 13:03:40 -0300

    Identificando o metodo HTTP

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,7 @@
 import http from 'node:http';//biblioteca nativa do node, para ultilizar o import eu tenho que adicionar o type module no package.json
 
 const server = http.createServer((request, response)=>{//criando um servidor
-    return response.end('John Frank')//enviando uma resposta
+    return response.end('Metodo:' + request)//enviando uma resposta do metodo da requisição "se é GET ou POST etc..."
 }) 
 
 server.listen(3333);//porta do servidor
