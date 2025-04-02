aede45aaa7321b4b8bcc70e2b7de1a3928c0ba06
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-04-02 15:41:52 -0300

    Recuperando dados no body
    Utilizamos o formato JSON para enviar os dados, e aprendemos a recuperar esses dados no Node.js, remontando os pedaços da requisição utilizando o buffer. Essa prática nos ajuda a entender o funcionamento interno das requisições antes de utilizar bibliotecas ou frameworks que abstraem esse processo. Essa compreensão é fundamental para lidar com requisições de forma mais avançada no futuro.

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,6 @@
 import http from 'node:http';//biblioteca nativa do node, para ultilizar o import eu tenho que adicionar o type module no package.json
 
-const server = http.createServer((request, response)=>{//criando um servidor
+const server = http.createServer(async(request, response)=>{//criando um servidor
     const { method, url } = request//metodo da requisição destruturado e a url do request
 
     if(method === 'GET' && url === '/products'){//se o metodo for GET e a url for /products
@@ -8,7 +8,16 @@ const server = http.createServer((request, response)=>{//criando um servidor
     }
 
     if (method === 'POST' && url === '/products'){//se o metodo for POST e a url for /products
+        const buffers = []//criando um buffer para armazenar os dados da requisição
+
+        for await (const chunk of request) {//enquanto houver dados na requisição
+            buffers.push(chunk)//adicionando os dados da requisição ao buffer
+        }
+
+        console.log(Buffer.concat(buffers).toString())//imprimindo o buffer que veio em padaços do emsomnia e montou no terminal / ultilizo o Buffer para transformar os dados em uma string ja montados no terminal
+
         return response.writeHead(201).end('Prodduto cadastrado!')//enviando uma resposta / quando eu defino o status code ele vai enviar o 201
+
     }
 
     return response.writeHead(404).end('Rota não encontrada')//quando não encontrar a rota ele vai enviar o 404  como erro
