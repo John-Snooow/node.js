52bd4e2d8d23c2f0228afee5200fb1b19a66a202
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-04-06 16:18:28 -0300

    Propriedade e método privado
    Foi mostrado como ajustar a visibilidade dos métodos e propriedades de uma classe em JavaScript. Foi explicado como tornar o banco de dados privado, limitando seu acesso apenas aos métodos da classe. Além disso, foi demonstrado como renomear e ajustar as referências ao banco de dados nos demais trechos de código. Ao final, foi ressaltada a importância de manter a organização e segurança do código, permitindo que cada método manipule o banco de dados de acordo com sua função específica.

--- api/src/database.js
+++ api/src/database.js
@@ -3,32 +3,32 @@ import { console } from "node:inspector";
 const DATABASE_PATH = new URL("db.json", import.meta.url);//pegando o caminho do banco de dados
 
 export class Database {
-  database = {};
+  #database = {};//criando um banco de dados os # indica que ele eh privado
 
   constructor() {//É executado automaticamente quando a classe é distanciada / 'const database = new Database() em routeHandler'
     fs.readFile(DATABASE_PATH, "utf-8")//lendo o banco de dados
       .then((data) => {
-        this.database = JSON.parse(data)//convertendo o banco de dados para JSON
+        this.#database = JSON.parse(data)//convertendo o banco de dados para JSON
     })
-    .catch(() => this.persist())
+    .catch(() => this.#persist())
   }
 
-  persist() {
-    fs.writeFile(DATABASE_PATH, JSON.stringify(this.database))//salvando o banco de dados
+  #persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database))//salvando o banco de dados
   }
 
   //insert PARA INSERIR
   insert(table, data) {
-    if (Array.isArray(this.database[table])) {
-      this.database[table].push(data);
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
     } else {
-      this.database[table] = [data];
+      this.#database[table] = [data];
     }
 
-    this.persist()
+    this.#persist()
   }
   //select PARA SELECIONAR
   select(table) {
-    return this.database[table] 
+    return this.#database[table] ?? []
   }
 }
