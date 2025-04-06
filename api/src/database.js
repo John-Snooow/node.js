d539ef2b3ded8ce6ec2697fc3cba96bfd613dbef
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-04-06 16:02:53 -0300

    Lendo dados no arquivo
    Processo de leitura de conteúdo de um arquivo para atualizar o banco de dados em memória. Foi mencionado o uso do fs (file system) para ler o arquivo, utilizando readfile e definindo o formato como tf8. A leitura assíncrona foi destacada, mostrando como recuperar o conteúdo do arquivo e convertê-lo em um objeto JSON. A importância de salvar e recuperar o conteúdo do arquivo para manter a consistência dos dados foi enfatizada.

--- api/src/database.js
+++ api/src/database.js
@@ -1,11 +1,16 @@
 import fs from "node:fs/promises"//biblioteca nativa do node
+import { console } from "node:inspector";
 const DATABASE_PATH = new URL("db.json", import.meta.url);//pegando o caminho do banco de dados
 
 export class Database {
   database = {};
 
   constructor() {//É executado automaticamente quando a classe é distanciada / 'const database = new Database() em routeHandler'
-    this.persist()
+    fs.readFile(DATABASE_PATH, "utf-8")//lendo o banco de dados
+      .then((data) => {
+        this.database = JSON.parse(data)//convertendo o banco de dados para JSON
+    })
+    .catch(() => this.persist())
   }
 
   persist() {
@@ -19,6 +24,8 @@ export class Database {
     } else {
       this.database[table] = [data];
     }
+
+    this.persist()
   }
   //select PARA SELECIONAR
   select(table) {
