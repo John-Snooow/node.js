cb20a343e7b32c1b87fff150b82094d90c08a5e9
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-04-06 15:36:54 -0300

    Criando arquivo
    Salva dados permanentemente em um arquivo usando Node.js. Foi criado um método construtor dentro de uma classe para instanciar um banco de dados dinamicamente. Foi utilizado o método persist para criar um arquivo de banco de dados, definindo o local do arquivo dinamicamente com base no endereço do arquivo atual. O File System (fs) do Node.js foi utilizado para manipular arquivos, escrevendo o conteúdo do banco de dados em um arquivo db.json.

--- api/src/database.js
+++ api/src/database.js
@@ -1,5 +1,16 @@
+import fs from "node:fs/promises"//biblioteca nativa do node
+const DATABASE_PATH = new URL("db.json", import.meta.url);//pegando o caminho do banco de dados
+
 export class Database {
   database = {};
+
+  constructor() {//É executado automaticamente quando a classe é distanciada / 'const database = new Database() em routeHandler'
+    this.persist()
+  }
+
+  persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.database))//salvando o banco de dados
+  }
 
   //insert PARA INSERIR
   insert(table, data) {
